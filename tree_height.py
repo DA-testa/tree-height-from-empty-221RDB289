57d727bf393437cb0ff3542c087f76b96f200e5f
221RDB289 <125434250+221RDB289@users.noreply.github.com>
2023-03-06 02:05:24 +0000

    changes

--- tree_height.py
+++ tree_height.py
@@ -1,4 +1,5 @@
 # python3
+# Autors: Gundars Pelle 6. grupa
 
 import sys
 import threading
@@ -7,21 +8,70 @@ import numpy
 
 def compute_height(n, parents):
     # Write this function
-    max_height = 0
     # Your code here
-    return max_height
+
+    v = parents # values
+    h = numpy.zeros(n) # heights
+    c = numpy.zeros(n) # checked nodes
+
+    i = 0
+    while 0 <= i and i <= n-1:
+        temp = []
+        j = i
+        increase = None
+        while(True):
+            if int(v[j]) == -1:
+                increase = 1
+                break
+            elif c[j] == 2:
+                increase = h[j]
+                break
+            elif c[j] == 1 or c[j] == 0:
+                temp.append(j)
+                for t in temp:
+                    if c[t] != 2:
+                        h[t] += 1
+                j = int(v[j])
+                
+        if increase:
+            for t in temp:
+                if c[t] == 1 or c[t] == 0:
+                    c[t] = 2
+                    h[t] += increase
+
+        i+=1
+
+
+    return int(numpy.max(h))
 
 
 def main():
     # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    mode = input()
+    if mode.startswith('I'):
+        # input number of elements
+        n = int(input())
+        # input values in one variable, separate with space, split these values in an array
+        values = input().split(' ')
+
+        # call the function and output it's result
+        print(compute_height(n, values))
+    elif mode.startswith('F'):
+        # let user input file name to use, don't allow file names with letter a
+        # account for github input inprecision
+        file_name = input()
+        if 'a' not in file_name:
+            f = open(file_name, 'r')
+            text = f.read().split('\n')
+            f.close()
+            # input number of elements
+            n = int(text[0])
+            # input values in one variable, separate with space, split these values in an array
+            values = text[1].split(' ')
+
+            # call the function and output it's result
+            print(compute_height(n, values))
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
